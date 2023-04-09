460661a7b01d41373ef0d88ddb071c015f2df1ed
Pranjul120568 <pranjuldwivedi120568@gmail.com>
2023-04-09 18:46:04 +0530

    db solved

--- box_management/settings.py
+++ box_management/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+from decouple import config
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -82,8 +86,12 @@ WSGI_APPLICATION = 'box_management.wsgi.application'
 #     }
 # }
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
 DATABASES = {
-    "default": dj_database_url.parse("postgres://spinnyapp_user:aN2BUCXILdRYCZwJ4yu2RCTrPINjDMZp@dpg-cgpac49euhlq285iv0g0-a.singapore-postgres.render.com/spinnyapp")
+    "default": dj_database_url.parse(DATABASE_URL)
+
 }
 
 AUTH_USER_MODEL = 'spinny.User'
